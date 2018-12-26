f27d3b1c2b0b4442c074ebf1360e1ada955def66
Mariano Ceballos <mariano.ceballos@gmail.com>
2018-12-26 12:20:20 -0300

    Migrations:   - Rename movements_categories to create_movements_categories   - Delete cash_register table, ya existe movements   - Delete items_table, ya existe movements_categories

--- backend/database/migrations/2018_12_25_151700_create_movements_categories.php
+++ backend/database/migrations/2018_12_25_151700_create_movements_categories.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MovementCategories extends Migration
+class CreateMovementsCategories extends Migration
 {
 
     public function up()
