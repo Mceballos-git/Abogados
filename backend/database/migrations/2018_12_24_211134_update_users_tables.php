bbbf9b267ba4ec5d7e5f31c2e44ace8b66fa1c92
Mariano Ceballos <mariano.ceballos@gmail.com>
2018-12-26 11:33:23 -0300

    Migrations:   - Fix update users table

--- backend/database/migrations/2018_12_24_211134_update_users_tables.php
+++ backend/database/migrations/2018_12_24_211134_update_users_tables.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
 
 class UpdateUsersTables extends Migration
 {
@@ -13,13 +14,14 @@ class UpdateUsersTables extends Migration
      */
     public function up()
     {
-        Schema::table('users', function ($table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('first_name');
             $table->string('last_name');
             $table->string('degree');
             $table->string('position');
             $table->timestamp('shift_start');
             $table->timestamp('shift_end');
+
         });
     }
 
@@ -30,7 +32,7 @@ class UpdateUsersTables extends Migration
      */
     public function down()
     {
-        Schema::table('users', function ($table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('first_name');
             $table->dropColumn('last_name');
             $table->dropColumn('degree');
