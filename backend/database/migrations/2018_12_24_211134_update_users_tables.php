c0651d0c0fddeb3fa7fc94e1c8056443154f5f63
Ezequiel Carrizo <ezequiel@scalecoding.com>
2019-01-02 17:45:48 +0000

    Fix en migration script

--- backend/database/migrations/2018_12_24_211134_update_users_tables.php
+++ backend/database/migrations/2018_12_24_211134_update_users_tables.php
@@ -14,15 +14,15 @@ class UpdateUsersTables extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('degree');
-            $table->string('position');
-            $table->string('shift_start');
-            $table->string('shift_end');
-
-        });
+//        Schema::table('users', function (Blueprint $table) {
+//            $table->string('first_name');
+//            $table->string('last_name');
+//            $table->string('degree');
+//            $table->string('position');
+//            $table->string('shift_start');
+//            $table->string('shift_end');
+//
+//        });
     }
 
     /**
@@ -32,13 +32,13 @@ class UpdateUsersTables extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('first_name');
-            $table->dropColumn('last_name');
-            $table->dropColumn('degree');
-            $table->dropColumn('position');
-            $table->dropColumn('shift_start');
-            $table->dropColumn('shift_end');
-        });
+//        Schema::table('users', function (Blueprint $table) {
+//            $table->dropColumn('first_name');
+//            $table->dropColumn('last_name');
+//            $table->dropColumn('degree');
+//            $table->dropColumn('position');
+//            $table->dropColumn('shift_start');
+//            $table->dropColumn('shift_end');
+//        });
     }
 }
