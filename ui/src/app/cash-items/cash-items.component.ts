af6d81d7fa12cba8bcb3e192aeb78ab6339ad8a3
Ezequiel Carrizo <ezequiel@scalecoding.com>
2019-01-03 16:56:40 +0000

    Arreglos en Tabla para usar Angular way (envez de serverside ya que no queremos ese tipo de comportamiento por ahora). Arreglos y ejemplos base para evitar errores de carga y esperar que cargue antes de intentar mostrar la tabla. Ejemplos de idioma.

--- ui/src/app/cash-items/cash-items.component.ts
+++ ui/src/app/cash-items/cash-items.component.ts
@@ -1,49 +1,79 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { MovementsService } from '../services/movements.service';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {MovementsService} from '../services/movements.service';
 
+import {Subject} from 'rxjs';
+import 'rxjs/add/operator/map';
 
-class Categories {
-  id: number;
-  name: string;
- }
+class Category {
+    id: number;
+    name: string;
+    created_at: string;
+    updated_at: string;
+}
 
 @Component({
-  selector: 'app-cash-items',
-  templateUrl: './cash-items.component.html',
-  styleUrls: ['./cash-items.component.css']
+    selector: 'app-cash-items',
+    templateUrl: './cash-items.component.html',
+    styleUrls: ['./cash-items.component.css']
 })
 export class CashItemsComponent implements OnInit {
 
-  dtOptions: DataTables.Settings = {};
-  items: Categories[];
+    dtOptions: DataTables.Settings = {};
+    dtTrigger: Subject<any> = new Subject();
+    loaded: boolean;
 
-  constructor(private http: HttpClient, private movService:MovementsService) { }
+    constructor(private http: HttpClient, private movService: MovementsService) {
+        this.loaded = false
+    }
 
-  ngOnInit(): void {
-    const that = this;
+    ngOnInit(): void {
+        this.movService.getMovementsCategoriesList().subscribe(response => {
+            this.buildDtOptions(response)
+            this.loaded = true;
+            this.dtTrigger.next();
+        });
+    }
 
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 2,
-      serverSide: true,
-      processing: true,
-      ajax: (dataTablesParameters: any, callback) => {
-        that.movService.getMovementsCategoriesList()          
-          .subscribe((resp:any) => {
-            that.items = resp;
-            console.log(resp);
-            
-            callback({
-              recordsTotal: resp.length,
-              recordsFiltered: resp.length,
-              data: []
-            });
-          });
-      },
-      columns: [{ data: 'id' }, { data: 'name' }]
-    };
-  }
+    private buildDtOptions(data: any): void {
+        this.dtOptions = {
+            language: {
+                processing: 'Procesando...',
+                lengthMenu: "Mostrar _MENU_ registros",
+                zeroRecords: "No se encontraron resultados",
+                emptyTable: "Ningún dato disponible en esta tabla",
+                info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+                infoFiltered: "(filtrado de un total de _MAX_ registros)",
+                infoPostFix: "",
+                search: "Buscar:",
+                url: "",
+                thousands: ",",
+                loadingRecords: "Cargando...",
+                paginate: {
+                    first: "Primero",
+                    last: "Último",
+                    next: "Siguiente",
+                    previous: "Anterior"
+                },
+                aria: {
+                    sortAscending: ": Activar para ordenar la columna de manera ascendente",
+                    sortDescending: ": Activar para ordenar la columna de manera descendente"
+                }
+            },
+            pagingType: 'full_numbers',
+            data: data,
+            columns: [
+                {title: 'ID', data: 'id'},
+                {title: 'Nombre', data: 'name'},
+            ]
+        };
+    }
+
+
+    ngOnDestroy(): void {
+        this.dtTrigger.unsubscribe();
+    }
 }
 
 
--- ui/src/app/services/movements.service.ts
+++ ui/src/app/services/movements.service.ts
@@ -1,29 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import 'rxjs/Rx';
-import { analyzeAndValidateNgModules } from '@angular/compiler';
+import {analyzeAndValidateNgModules} from '@angular/compiler';
 
 class DataTablesResponse {
-  data: any[];
-  draw: number;
-  recordsFiltered: number;
-  recordsTotal: number;
+    data: any[];
+    draw: number;
+    recordsFiltered: number;
+    recordsTotal: number;
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class MovementsService {
 
-  constructor(private http: HttpClient) { 
+export class MovementsService{
 
-  }
+    constructor(private http: HttpClient) {
 
-  getMovementsCategoriesList(){
-    return this.http.get<DataTablesResponse>('http://local.sassani.com/rubros');  
-  }
+    }
 
-
-
+    getMovementsCategoriesList() {
+        return this.http.get('http://local.abogados.com/rubros');
+    }
 
 }
